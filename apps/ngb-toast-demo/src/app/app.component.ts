dc16540e59ac77be9a7ae45a0bbf242673ae4a85
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-12-29 20:36:55 +0100

    Scaffolded demo app

---
+++ apps/ngb-toast-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-bootstrap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngb-toast-demo';
+}
